3581a2d30334368b19ffc16932c293044a737fcc
unknown <nikhilppry2001@gmail.com>
2023-08-22 11:30:39 +0530

    first commit

---
+++ nik.js
@@ -0,0 +1,11 @@
+function cylinderSurfaceArea(r,h){
+   const π= Math.PI;
+ const lateralSurfaceArea=2*π*r*h;
+ const baseArea=2* π * r ** 2;
+  total=lateralSurfaceArea+baseArea;
+}
+const r= 20;
+const h= 30
+    
+cylinderSurfaceArea(r,h)   
+    console.log(total);
