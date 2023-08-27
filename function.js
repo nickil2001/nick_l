032bbd6daa453da6e780752204c455a886201b1e
nickil2001 <142251816+nickil2001@users.noreply.github.com>
2023-08-27 20:08:26 +0530

    function by using return

--- function.js
+++ function.js
@@ -1,9 +1,24 @@
-let π=3.14159265359
-let r=20
-let d=4
- console.log("equation =4πr^2")
-function spherearea(a,b,c){
-    console.log("sphere area=",a*b*c)
+var readlinesync = require("readline-sync");
+let money = readlinesync.question("Enter the money:");
+const Onekgapple = 100;
+const Onekgorenge = 200;
+const RambutanONEKG = 400;
+const pineappleONEKG = 200;
+const mangoONEKG = 500;
+const PapayaONEKG = 300;
+const PeachONEKG = 600;
+
+console.log("Total Fruits = 12");
+
+function calculateTotalCost(a, b, c, d, e, f, g) {
+    const totalcost = a + b + c + d + e + f + g;
+    return totalcost;
 }
 
-spherearea(d,π,r**2)
+const totalCost = calculateTotalCost(Onekgapple, Onekgorenge, RambutanONEKG, pineappleONEKG, mangoONEKG, PapayaONEKG, PeachONEKG);
+
+if (money >= totalCost) {
+    console.log("You can buy all the fruits");
+} else {
+    console.log("You cannot buy all the fruits.");
+}
